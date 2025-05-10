cba8418adadcf486898174acfc4ac3565c6d1d2d
srhkami <srhkami@gmail.com>
2025-05-10 08:54:24 +0800

    1140509: 調整簡介格式

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import Nav from "./component/Layout/Nav.tsx";
 import Footer from "./component/Layout/Footer.tsx";
 import ModalUpload from "./component/ImageUpload/ModalUpload.tsx";
 import ModalOutput from "./component/ImageOutput/ModalOutput.tsx";
+import {BsFillPersonLinesFill} from "react-icons/bs";
+import {MdNumbers} from "react-icons/md";
 
 
 function App() {
@@ -26,9 +28,10 @@ function App() {
                 <div className="badge badge-md badge-accent my-3">Step 1</div>
                 <div className='col-span-3 text-lg font-bold flex items-center'>
                   點擊左下角
-                  <button className='btn btn-sm btn-primary ml-2'>
+                  <button className='btn btn-sm btn-primary btn-outline mx-2'>
                     新增圖片
                   </button>
+                  按鈕
                 </div>
                 <div className="badge badge-md badge-accent my-3">Step 2</div>
                 <div className='col-span-3 text-lg font-bold flex items-center'>
@@ -37,22 +40,30 @@ function App() {
                 <div className="badge badge-md badge-accent my-3">Step 3</div>
                 <div className='col-span-3 text-lg font-bold flex items-center'>
                   點擊右下角
-                  <button className='btn btn-sm btn-success ml-2'>
+                  <button className='btn btn-sm btn-success mx-2 btn-outline'>
                     輸出檔案
                   </button>
+                  按鈕
                 </div>
                 <div className='col-span-4 text-lg font-bold mt-3'>
                   恭喜您完成一份照片黏貼表！
                 </div>
               </div>
-              <div className='divider divider-info'></div>
-              <div className='grid grid-cols-4 gap-2 '>
-                <div>作者：</div>
+              <div className='divider'></div>
+              <div className='grid grid-cols-4 gap-2 font-bold text-gray-300'>
+                <div className='flex justify-start items-center'>
+                  <BsFillPersonLinesFill className='mr-2'/>
+                  作者
+                </div>
                 <div className='col-span-3 text-start'>蔡智楷 C.K.SAI</div>
-              </div>
-              <div className='grid grid-cols-4 gap-2 '>
-                <div>版本：</div>
-                <div className='col-span-3 text-start'>1140501</div>
+                <div className='flex justify-start items-center'>
+                  <MdNumbers className='mr-2'/>
+                  版本
+                </div>
+                <div className='col-span-3 text-start'>1.0_1140510</div>
+                <div className='col-span-4 flex justify-end'>
+                  本軟體分享於 <a href="https://trafficpigeon.com/" className="link link-primary" target='_blank'>交通鴿手</a>
+                </div>
               </div>
             </div>
           </div>
